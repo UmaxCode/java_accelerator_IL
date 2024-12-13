72e81008509f542d3c88d3a20450584146917fd3
umaxcode <maxwellodoom1729@gmail.com>
2024-12-13 11:32:54 +0000

    feat: implement shape creation with factory method pattern.

---
+++ src/main/java/org/umaxcode/designPatterns/creational/factoryMethod/product/impl/Circle.java
@@ -0,0 +1,22 @@
+package org.umaxcode.designPatterns.creational.factoryMethod.product.impl;
+
+import org.umaxcode.designPatterns.creational.factoryMethod.product.Shape;
+
+public class Circle implements Shape {
+
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
