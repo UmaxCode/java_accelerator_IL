3b7a9b3adcd065d1b4d3f925bbbd43c5ab9ab10d
umaxcode <maxwellodoom1729@gmail.com>
2024-12-30 12:56:42 +0000

    feat: add config server

---
+++ src/main/java/org/umaxcode/microservices/config-service/src/test/java/com/umaxcode/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umaxcode.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
