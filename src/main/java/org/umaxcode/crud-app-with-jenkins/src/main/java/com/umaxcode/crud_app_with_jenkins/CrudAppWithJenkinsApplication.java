456571e73ca79f16147fbbc6da3b6a8739e1732a
umaxcode <maxwellodoom1729@gmail.com>
2025-01-27 10:14:55 +0000

    chore: initial project setup

---
+++ src/main/java/org/umaxcode/crud-app-with-jenkins/src/main/java/com/umaxcode/crud_app_with_jenkins/CrudAppWithJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.umaxcode.crud_app_with_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppWithJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAppWithJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/umaxcode/crud-app-with-jenkins/src/test/java/com/umaxcode/crud_app_with_jenkins/CrudAppWithJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umaxcode.crud_app_with_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppWithJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
